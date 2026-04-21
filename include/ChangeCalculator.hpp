9f535a30a1589f2a08e2945899e77543908625f9
Manarith <Rafal.fraczek5@gmail.com>
2026-04-21 15:43:14 +0200

    Added change calculation

---
+++ include/ChangeCalculator.hpp
@@ -0,0 +1,11 @@
+#ifndef CHANGECALCULATOR_HPP
+#define CHANGECALCULATOR_HPP
+
+#include <map>
+
+class ChangeCalculator {
+public:
+    static std::map<double, int> calculate(double amount, std::map<double, int>& cash);
+};
+
+#endif
---
+++ src/server/ChangeCalculator.cpp
@@ -0,0 +1,12 @@
+#include "ChangeCalculator.hpp"
+#include "CashRegister.hpp"
+
+std::map<double, int> ChangeCalculator::calculate(double amount, std::map<double, int>& cash) {
+    CashRegister temp;
+    for (auto& [v, c] : cash) temp.add(v, c);
+
+    if (!temp.canProvide(amount))
+        return {};
+
+    return temp.withdrawChange(amount);
+}
